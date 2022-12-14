6e99df6adf559a3328843d250c6ea60274c23a02
sowmyatata <sowmyatata42@gmail.com>
2022-12-14 19:34:14 +0000

    First Commit

---
+++ AzureWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AzureWebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
